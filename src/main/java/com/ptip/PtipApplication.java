0f46b9f9012d5a811dc8680e7ca7b020aaa145e8
JYL35 <dlwodbs1223@naver.com>
2024-09-28 15:08:34 +0900

    Create SpringBoot Project

---
+++ src/main/java/com/ptip/PtipApplication.java
@@ -0,0 +1,13 @@
+package com.ptip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptip/PtipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
