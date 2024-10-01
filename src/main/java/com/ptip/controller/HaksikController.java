27272d7e369c9249657ab3772e50cd865997bf91
taco-recipe <taco9590@gmail.com>
2024-10-02 02:13:11 +0900

    haksik 기능 구현

---
+++ src/main/java/com/ptip/controller/HaksikController.java
@@ -0,0 +1,56 @@
+package com.ptip.controller;
+
+
+import com.ptip.models.Haksik;
+import com.ptip.service.HaksikService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/haksik")
+public class HaksikController {
+
+    private HaksikService haksikService;
+
+    private ZoneId koreaZoneId = ZoneId.of("Asia/Seoul");
+
+    // 현재 시간 (한국 시간)
+    private ZonedDateTime nowInKorea = ZonedDateTime.now(koreaZoneId);
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    public HaksikController(HaksikService haksikService) {
+        this.haksikService = haksikService;
+    }
+
+    @GetMapping("/{date}")
+    public List<Haksik> byDate(@PathVariable("date") String date){
+        System.out.println(date);
+        return haksikService.getHaksikByDate(date);
+    }
+
+    @GetMapping("/today")
+    public List<Haksik> todayHaksik(){
+        LocalDate today = nowInKorea.toLocalDate();
+        return haksikService.getHaksikByDate(today.format(dateFormatter));
+    }
+
+    @GetMapping("/thisweek")
+    public List<Haksik> thisWeekHaksik(){
+        LocalDate today = nowInKorea.toLocalDate();
+        LocalDate monday =  today.with(DayOfWeek.MONDAY);
+        LocalDate friday = today.with(DayOfWeek.FRIDAY);
+        return haksikService.getHaksikbyRange(monday.format(dateFormatter),friday.format(dateFormatter));
+
+    }
+
+}
