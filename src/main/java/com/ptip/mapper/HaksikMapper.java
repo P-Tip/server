658f2a9503bf242e366392fd331472215374cb38
taco-recipe <taco9590@gmail.com>
2024-11-06 11:35:04 +0900

    haksik mapper 추가 수정

--- src/main/java/com/ptip/mapper/HaksikMapper.java
+++ src/main/java/com/ptip/mapper/HaksikMapper.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 @Mapper
 public interface HaksikMapper {
-    @Select("SELECT * FROM test_db.haksik WHERE date = #{date} ")
+    @Select("SELECT * FROM haksik.haksik WHERE date = #{date} ")
     List<Haksik> getHaksikByDate(@Param("date") String date);
 
-    @Select("SELECT * FROM test_db.haksik WHERE #{startdate} <= date && date <= #{enddate} ORDER BY date ASC")
+    @Select("SELECT * FROM haksik.haksik WHERE #{startdate} <= date && date <= #{enddate} ORDER BY date ASC")
     List<Haksik> getHaksikByRange(@Param("startdate") String startdate, @Param("enddate") String enddate);
 }
