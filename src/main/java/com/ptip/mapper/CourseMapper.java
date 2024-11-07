64d29fc01daf88aa8beffd836697c3b62ab969fb
JYL35 <dlwodbs1223@naver.com>
2024-11-07 12:04:40 +0900

    Update CourseMapper.java

--- src/main/java/com/ptip/mapper/CourseMapper.java
+++ src/main/java/com/ptip/mapper/CourseMapper.java
@@ -11,17 +11,17 @@ import java.util.List;
 public interface CourseMapper {
 
     @Select("SELECT course_no, title, course_type, course_time, classroom, name as major, professor " +
-            "FROM course join major on course.major_id = major.id " +
+            "FROM course_schedule.course join course_schedule.major on course.major_id = major.id " +
             "WHERE REPLACE(course_no, '-', '') LIKE CONCAT('%', REPLACE(#{course_no}, ' ', ''), '%')")
     List<Course> selectByCourseNo(@Param("course_no") String course_no);
 
     @Select("SELECT course_no, title, course_type, course_time, classroom, name as major, professor " +
-            "FROM course join major on course.major_id = major.id " +
+            "FROM course_schedule.course join course_schedule.major on course.major_id = major.id " +
             "WHERE TITLE LIKE CONCAT('%', REPLACE(#{title}, ' ', ''), '%')")
     List<Course> selectByTitle(@Param("title") String title);
 
     @Select("SELECT course_no, title, course_type, course_time, classroom, name as major, professor " +
-            "FROM course left join major on course.major_id = major.id " +
+            "FROM course_schedule.course left join course_schedule.major on course.major_id = major.id " +
             "WHERE course_type LIKE CONCAT('%', '교', '%')")
     List<Course> selectByCourseType();
 
