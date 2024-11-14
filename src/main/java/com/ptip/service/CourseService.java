bff14f6ae71771075eccf3517adcb23ee391890f
kimin3 <kmns0829@gmail.com>
2024-11-15 00:56:09 +0900

    Update code

--- src/main/java/com/ptip/service/CourseService.java
+++ src/main/java/com/ptip/service/CourseService.java
@@ -17,43 +17,12 @@ public class CourseService {
         this.courseMapper = courseMapper;
     }
 
-    // 학수번호 검색
-    public List<Course> searchByCourseNo(String courseNo) {
-        return courseMapper.selectByCourseNo(courseNo);
-    }
-
-    // 강의명 검색
-    public List<Course> searchByTitle(String title) {
-        return courseMapper.selectByTitle(title);
-    }
-
-    // 교수명 검색
-    public List<Course> searchByProfessor(String professor) {
-        return courseMapper.selectByProfessor(professor);
-    }
-
-    // 해당 학점으로 찾기
-    public  List<Course> findByCredit(int credit) {
-        return courseMapper.selectByCredit(credit);
-    }
-
-    // 해당 학년으로 찾기
-    public  List<Course> findByGrade(int grade) {
-        return courseMapper.selectByGrade(grade);
+    public  List<Course> searchCourse(String title, String professor, String courseNo, String credit, String grade, String courseType, String major) {
+        return courseMapper.selectCourse(title, professor, courseNo, credit, grade, courseType, major);
     }
 
     // 강의 시간으로 찾기
     public  List<Course> findByTime(String day, int startTime, int endTime) {
         return courseMapper.selectByTime(day, startTime, endTime);
-    }
-
-    // 전공/영역으로 필터링
-    public List<Course> filterByCourseType(String courseType) {
-        return courseMapper.selectByCourseType(courseType);
-    }
-
-    // 전공/영역으로 필터링
-    public List<Course> filterByMajor(String major) {
-        return courseMapper.selectByMajor(major);
     }
 }
