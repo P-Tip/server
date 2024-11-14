4bfe6e2b000129c1ea4b88296b094342e16bb621
JYL35 <dlwodbs1223@naver.com>
2024-11-14 12:49:25 +0900

    Add filterByfirstConsonant & Rename Award's Controller, Service, Mapper

--- src/main/java/com/ptip/mapper/AwardMapper.java
+++ src/main/java/com/ptip/mapper/AwardMapper.java
@@ -1,5 +1,6 @@
 package com.ptip.mapper;
 
+import com.ptip.models.Department;
 import com.ptip.models.Program;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-public interface ProgramMapper {
+public interface AwardMapper {
 
     //id 조회
     @Select("SELECT p.id, p.program_name, p.contents, " +
@@ -20,6 +21,29 @@ public interface ProgramMapper {
             "WHERE (p.id = #{id} OR #{id} IS NULL)" +
             "ORDER BY p.id")
     List<Program> searchById(@Param("id") Integer id);
+
+    //부서 조회
+    @Select("<script>" +
+            "SELECT department_name, location, internal_num " +
+            "FROM department " +
+            "WHERE CASE " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '가' AND '깋' THEN 'ㄱ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '나' AND '닣' THEN 'ㄴ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '다' AND '딯' THEN 'ㄷ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '라' AND '맇' THEN 'ㄹ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '마' AND '밓' THEN 'ㅁ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '바' AND '빟' THEN 'ㅂ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '사' AND '싷' THEN 'ㅅ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '아' AND '잏' THEN 'ㅇ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '자' AND '짛' THEN 'ㅈ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '차' AND '칳' THEN 'ㅊ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '카' AND '킿' THEN 'ㅋ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '타' AND '팋' THEN 'ㅌ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '파' AND '핗' THEN 'ㅍ' " +
+            "    WHEN LEFT(department_name, 1) BETWEEN '하' AND '힣' THEN 'ㅎ' " +
+            "    ELSE '' END = #{consonant}" +
+            "</script>")
+    List<Department> filterByFirstConsonant(@Param("consonant") String consonant);
 
     // 통합 검색(MyBatis용 SQL문)
     @Select("<script>" +
