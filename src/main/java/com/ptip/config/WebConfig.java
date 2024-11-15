49debd844c3d3b5b63817dadc8cfdc1e67b1562e
JYL35 <dlwodbs1223@naver.com>
2024-11-15 21:10:18 +0900

    Update WebConfig.java

--- src/main/java/com/ptip/config/WebConfig.java
+++ src/main/java/com/ptip/config/WebConfig.java
@@ -10,14 +10,18 @@ import org.springframework.context.annotation.Configuration;
 public class WebConfig {
 
     @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
-        return factory -> {
-            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-            connector.setScheme("http");
-            connector.setPort(80);
-            connector.setSecure(false);
-            connector.setRedirectPort(443); // HTTPS 포트로 리다이렉트
-            factory.addAdditionalTomcatConnectors(connector);
-        };
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addAdditionalTomcatConnectors(createHttpConnector());
+        return factory;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443); // HTTPS 포트로 리다이렉트
+        return connector;
     }
 }
