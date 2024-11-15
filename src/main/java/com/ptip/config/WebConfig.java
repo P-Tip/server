461f7a1e188cc7bdac927b99012d60c8d9bbcb49
JYL35 <dlwodbs1223@naver.com>
2024-11-15 16:29:43 +0900

    add redriect code

--- src/main/java/com/ptip/config/WebConfig.java
+++ src/main/java/com/ptip/config/WebConfig.java
@@ -1,55 +1,23 @@
 package com.ptip.config;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("forward:/index.html");
-//    }
+public class WebConfig {
 
     @Bean
-    public RedirectFilter redirectFilter() {
-        return new RedirectFilter();
-    }
-
-    // HTTP -> HTTPS 리디렉션을 위한 필터
-    public static class RedirectFilter implements Filter {
-
-        @Override
-        public void init(FilterConfig filterConfig) throws ServletException {
-        }
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response,
-                             FilterChain chain) throws java.io.IOException, ServletException {
-            if (request instanceof HttpServletRequest) {
-                HttpServletRequest req = (HttpServletRequest) request;
-                HttpServletResponse res = (HttpServletResponse) response;
-                if ("http".equalsIgnoreCase(req.getScheme())) {
-                    String newUrl = "https://" + req.getServerName() + req.getRequestURI();
-                    res.sendRedirect(newUrl);
-                    return;
-                }
-            }
-            chain.doFilter(request, response);
-        }
-
-        @Override
-        public void destroy() {
-        }
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return factory -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setScheme("http");
+            connector.setPort(80);
+            connector.setSecure(false);
+            connector.setRedirectPort(443); // HTTPS 포트로 리다이렉트
+            factory.addAdditionalTomcatConnectors(connector);
+        };
     }
 }
