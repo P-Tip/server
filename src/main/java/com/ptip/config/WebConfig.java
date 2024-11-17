2f2001f5c111e607d56a72ff787c9e1dd2e0cd7d
JYL35 <dlwodbs1223@naver.com>
2024-11-17 13:19:42 +0900

    Update WebConfig.java (about cors)

--- src/main/java/com/ptip/config/WebConfig.java
+++ src/main/java/com/ptip/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("https://ptutip.p-e.kr")// Azure Static Web App URL
+                .allowedOrigins("https://www.ptutip.p-e.kr")// Azure Static Web App URL
                 .allowedMethods("GET")
                 .allowedHeaders("*")
                 //.allowCredentials(true)  // 쿠키 인증 정보 허용
