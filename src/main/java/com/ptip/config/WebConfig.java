21ba96ead9ff9088bc4f079e2c1a9afcbe270d13
JYL35 <dlwodbs1223@naver.com>
2024-11-17 12:48:29 +0900

    Update WebConfig.java (about cors)

--- src/main/java/com/ptip/config/WebConfig.java
+++ src/main/java/com/ptip/config/WebConfig.java
@@ -7,9 +7,19 @@ import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class WebConfig {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해
+                .allowedOrigins("https://ptutip.p-e.kr")// Azure Static Web App URL
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                //.allowCredentials(true)  // 쿠키 인증 정보 허용
+                .maxAge(3600);  // preflight 캐시 시간
+    }
 
     @Bean
     public TomcatServletWebServerFactory servletContainer() {
