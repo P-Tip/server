2d3c0031b46416fcdee5ea26e38012f247d45bff
kimin3 <kmns0829@gmail.com>
2025-03-27 18:58:12 +0900

    fix code

--- src/main/java/com/ptip/auth/config/SecurityConfig.java
+++ src/main/java/com/ptip/auth/config/SecurityConfig.java
@@ -63,8 +63,7 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests((auth) -> auth   // 경로별 인가 작업
-                        .requestMatchers("/", "/api/auth/**", "api/award/**").permitAll()
-                        .requestMatchers("/api/admin").hasRole("ROLE_ADMIN")
+                        .requestMatchers("/", "/api/**").permitAll()
                 )
 
         //세션 설정 : STATELESS
